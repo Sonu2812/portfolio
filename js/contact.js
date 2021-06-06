72917ce52745a895f08b9d2d7eccfaa90307a8c8
Sonu2812 <goswamisonu378@gmail.com>
2021-06-06 12:55:04 +0530

    portfolio

--- js/contact.js
+++ js/contact.js
@@ -5,15 +5,35 @@ function validation(){
 
     var comment_box = document.getElementById("comment_box").value;
 
+
     if(first == ""){
-        document.getElementById("Name").innerHTML = "User name is required";
+        document.getElementById("Name").innerHTML = " ** User name is required";
         return false;
     }
+    if((first.length <= 2) || (first.length > 20)){
+        document.getElementById("Name").innerHTML =" ** Your name length must be between 2 to 20";
+        return false;
+    }
+    if(!isNaN(first)){
+        document.getElementById("Name").innerHTML =" ** Only characters are allowed";
+        return false;
+    }
+
 
     if(last == ""){
         document.getElementById("email").innerHTML = "Valid e mail address is required";
         return false;
     }
+    if(last.indexOf('@') <= 0){
+        document.getElementById("email").innerHTML =" ** @ Invalid Position";
+        return false;
+    }
+    if((last.charAt(last.length-4)!='.') && (last.charAt(last.length-3)!='.')){
+        document.getElementById("email").innerHTML =" ** .Invalid Position";
+        return false;
+    }
+
+
 
     if(comment_box == ""){
         document.getElementById("comment").innerHTML = "Please Enter Some text's";
